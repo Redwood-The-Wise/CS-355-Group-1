fcaa5f47e8df1d2a4ebd8272e339e51885825809
LoganNoles <lnoles1@una.edu>
2025-03-14 00:37:28 -0500

    Update CombatPlayer.cpp

--- CombatPlayer.cpp
+++ CombatPlayer.cpp
@@ -21,13 +21,61 @@
 
 CombatPlayer::CombatPlayer()
 {
-
+	
 }
-void CombatPlayer::attack()
+// *************************************************************************
+// attack is a public method method to deal with the attacks.
+//
+// Incoming Data: EnemyNPC* enemy
+// Outgoing Data: None
+// *************************************************************************
+// *************************************************************************
+// Edit Log
+// *************************************************************************
+// *************************************************************************
+// Name: Logan Noles
+// Date: 3/13/2025
+// Description: Created attack and added the check for the enemy being near.
+// *************************************************************************
+void CombatPlayer::attack(EnemyNPC* enemy)
 {
-
+	if (true) //we need a method to check if the enemy is near.
+	{
+		initiateCombat(enemy);
+	}
+	else
+	{
+		cout << "You swing wildly at the air." << endl;
+	}
 }
 void CombatPlayer::equip()
 {
 
+}
+// *************************************************************************
+// initiateCombat is a private helper method to deal with the math for the combat.
+//
+// Incoming Data: EnemyNPC* enemy
+// Outgoing Data: None
+// *************************************************************************
+// *************************************************************************
+// Edit Log
+// *************************************************************************
+// *************************************************************************
+// Name: Logan Noles
+// Date: 3/13/2025
+// Description: Created initiateCombat.
+// *************************************************************************
+void CombatPlayer::initiateCombat(EnemyNPC* enemy)
+{
+	//for balance reasons, we check if our armor is greater than our damage.
+	//if it is, we deal 1 damage to the enemy, else we deal the difference between our damage and armor.
+	if (armor > damage)
+	{
+		enemy->getStats()->health -= 1;
+	}
+	else
+	{
+		enemy->getStats()->health -= damage - armor;
+	}
 }
